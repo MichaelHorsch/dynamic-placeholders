b92a48806864af929e1b91f852c12e0dffe46272
MichaelHorsch <mhorsch@gmail.com>
2017-03-29 14:53:42 -0500

    Fixing so it works with multiple placeholders at the same level.

--- Source/DynamicPlaceholders/PlaceholderKeyRegex.cs
+++ Source/DynamicPlaceholders/PlaceholderKeyRegex.cs
@@ -3,6 +3,6 @@
     public static class PlaceholderKeyRegex
     {
         public static string DynamicKeyRegex = @"(.+)_[\d\w]{8}\-(?:[\d\w]{4}\-){3}[\d\w]{12}(?:_\d+)?";
-        public static string AllowedRenderingsDynamicKeyRegex = @"(_([A-Fa-f0-9]{8}(-[A-Fa-f0-9]{4}){3}-[A-Fa-f0-9]{12}))";
+        public static string AllowedRenderingsDynamicKeyRegex = @"(_([A-Fa-f0-9]{8}(-[A-Fa-f0-9]{4}){3}-[A-Fa-f0-9]{12})(_[\d])*)";
     }
 }
